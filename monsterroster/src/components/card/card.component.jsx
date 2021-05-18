879de9632b30dc8b65bb082710dc7af451de375b
codyalmand <codybalmand@gmail.com>
2021-05-18 16:10:41 -0500

    added site url to get cartoon images

--- monsterroster/src/components/card/card.component.jsx
+++ monsterroster/src/components/card/card.component.jsx
@@ -3,6 +3,10 @@ import '../card/card.component.css'
 
 export const Card = props => (
     <div className = 'card-container'>
+        <img 
+            alt = 'Monster' 
+            src = {`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
+        />
         <h1>{props.monster.name}</h1>
     </div>
 );
